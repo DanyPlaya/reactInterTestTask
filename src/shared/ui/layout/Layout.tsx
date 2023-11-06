a9cde5f0187626b89cd87dbff08aeb207babf6a5
DanyPlaya <danila.lusuy@gmail.com>
2023-11-06 19:55:21 +0300

    feat: created a universal layout

---
+++ src/shared/ui/layout/Layout.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+import cls from "./Layout.module.scss";
+import { Outlet, ScrollRestoration } from "react-router-dom";
+type LayoutProps = {
+  navbarSlot?: ReactNode;
+  headerSlot?: ReactNode;
+  bottomSlot?: ReactNode;
+};
+export const Layout = (props: LayoutProps) => {
+  const { bottomSlot, headerSlot, navbarSlot } = props;
+  return (
+    <div className={cls.root}>
+      <header>
+        {navbarSlot}
+        {headerSlot}
+      </header>
+      <div className={cls.container}>
+        <div className={cls.content}>
+          <Outlet />
+          <ScrollRestoration />
+        </div>
+      </div>
+      <footer className={cls.footer}>
+        <a href="https://github.com/DanyPlaya/reactInterTestTask">
+          https://github.com/DanyPlaya/reactInterTestTask
+        </a>
+      </footer>
+    </div>
+  );
+};
